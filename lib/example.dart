654220bfec4d660474ac111629467f57660c2496
CoderRedwing <ajiteshru@gmail.com>
2022-12-22 20:06:57 +0530

    m1

---
+++ lib/example.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+class Example extends StatefulWidget {
+  const Example({Key? key}) : super(key: key);
+
+  @override
+  _ExampleState createState() => _ExampleState();
+}
+
+    class _ExampleState extends State<Example> {
+  List<Photos> photoList = [];
+  Future<List<Photos>> getPhotos () async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+    var data =jsonDecode(response.body.toString());
+    for(Map i in data){
+      Photos photos= Photos(title: i['title'], url: i['url']);
+      photoList.add(photos);
+    }
+    if(response.statusCode==200){
+     return photoList;
+    }else{
+      return photoList;
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title:  const Text("API Course"),
+      ),
+      body: Column(
+        children: [
+       Expanded(
+         child: FutureBuilder(
+           future: getPhotos(),
+             builder:  (context, AsyncSnapshot<List<Photos>> snapshot){
+           return ListView.builder(
+             itemCount: photoList.length,
+               itemBuilder: (context ,index){
+            return  ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(snapshot.data![index].url.toString()),
+              ),
+              title: Text(snapshot.data![index].title.toString()),
+            );
+           });
+    }),
+       )
+        ],
+      ),
+    );
+  }
+}
+class Photos{
+  String title ,url;
+  Photos({required this.title, required this.url});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/example.dart';
 import 'package:untitled/home_s.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const Example(),
                  );
   }
 }
